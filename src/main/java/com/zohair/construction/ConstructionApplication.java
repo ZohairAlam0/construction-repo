e8938686519112851823dbd5cec981a37d6388f6
ELAB206-02\Administrator <daniyalnawaz@iqra.edu.pk>
2024-11-21 16:47:12 +0500

    lombok dependency

--- src/main/java/com/zohair/construction/ConstructionApplication.java
+++ src/main/java/com/zohair/construction/ConstructionApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ConstructionApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ConstructionApplication.class, args);
 	}
 
